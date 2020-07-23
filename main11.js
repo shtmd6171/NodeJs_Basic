4837dbe619160bfc7a51516497a594593718a87e
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-24 00:55:24 +0900

    sanitize-html을 이용한 출력정보 보안과 옵션

--- main11.js
+++ main11.js
@@ -3,12 +3,12 @@
 //그리고 아래와 같은 방식으로 require 형식으로 객체 내부의 함수를 받아와
 // 사용하는 방법에 대해 설명하고 있다.
 
-// main10.js ) 이 Js파일 내부에서는 main8.js와는 달리
+// main10.js ) 이 Js파일 내부에서는 main9.js와는 달리
 // 입력 정보의 보안 문제와 관련된 문제와 내용의 대해 설명하고 있다.
 // 1. password.js를 생성하고 사용자 정보를 저장할 때,
 //    URIpath를 통해서 사용자가 해당 파일에 직접 접근해버릴 수 있다.
 
-// + ) 이 Js파일 내부에서는 main8.js와는 달리
+// + ) 이 Js파일 내부에서는 main10.js와는 달리
 // 출력 정보의 보안 문제와 관련된 문제와 내용의 대해 설명하고 있다.
 // 1. 만약 web에서 create페이지를 통해 writeFile함수를 통한 data를 생성했을 때
 // <script>alert('data')</script> 와 같은 형식으로 html 내부에서
@@ -23,13 +23,18 @@
 // 모듈이 설치되면 node_modules 파일이 생성된다.
 // 또한 packge에 "dependencies": {  "sanitize-html": "^1.27.1" }가 추가된 것을 볼 수 있다.
 
+// + ) 후술되는 코드를 이용하고 XSS 웹 페이지를 불러오면 main10.js에서 뜨는 얼럿이
+// sanitize된 main11.js에서는 등장하지 않게 된다.
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var path = require('path');
 // main9.js ) template의 변수에 template.js의 객체를 받았다.
-var template = require('./lib/template.js')
+var template = require('./lib/template.js');
+// + ) sanitize module 사용 require문
+var sanitizeHtml = require('sanitize-html');
 
 function create(addr) {
   return fs.readFileSync(addr, 'utf8');
@@ -57,20 +62,37 @@ var app = http.createServer(function(request, response) {
       });
     } else {
       fs.readdir('./data', function(error, filelist) {
-        var title = queryData.id;
-        var list = template.List(filelist);
         // main10.js ) 지정 경로가 아닌 path정보의 base부분만을
         //추출해서 변수에 삽입해서 URI를 통한 다른 파일 열람을 막을 수 있다.
         var filteredID = path.parse(queryData.id).base;
         // main10.js ) readFile내부의 data/${queryData.id}를 ./lib/password.js로
         // 접근하면 중요 파일을 열람해버리는 문제가 생긴다.
         fs.readFile(`data/${filteredID}`, 'utf8', function(err, data) {
-          var HTML = template.Html(title, list,
-            `<h2>${title}</h2><p>${data}</p>`,
+          // + ) 기본 문법 var dirty = 'some really tacky HTML';
+          // var clean = sanitizeHtml(dirty);
+          // 타이틀과 본문 내용을 검열한다.
+          // Q. 왜 이 부분을 sanitize 할까 ?
+          // A. 이 부분이 파일을 불러와 본문을 출력해주는 부분이다.
+          // 정보 출력시 문제가 있다면, 본문을 출력하는 이 내용에 문제가 있는 것이므로
+          // 페이지를 표시 해주는 내용을 검열해주면 된다.
+
+          // + ) filteredID를 통해 readfile의 파일경로를 지정 했으므로
+          // title이 readFile 밖에서 선언될 필요가 없으므로
+          // title을 readFile 안쪽으로 이동시켰다 (list도 같이).
+          var title = queryData.id;
+          var list = template.List(filelist);
+          var sanitizedTitle = sanitizeHtml(title);
+          // allowedTags는 XSS파일 내에 <h1>태그를 허용하기 위해 사용했다.
+          // 자세한 사용 내용은 https://www.npmjs.com/package/sanitize-html
+          var sanitizedData = sanitizeHtml(data, {
+            allowedTags : ['h1']
+          });
+          var HTML = template.Html(sanitizedTitle, list,
+            `<h2>${sanitizedTitle}</h2><p>${sanitizedData}</p>`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="http://localhost:3000/delete_process" method="post">
-            <input type="hidden" name="id" value="${title}">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
             </form> `);
           response.writeHead(200);
